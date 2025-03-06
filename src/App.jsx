8600919a3b19051141fa40946f7a14fca0535f0f
akghosh111 <ghoshkiran2017@gmail.com>
2025-03-06 22:24:46 +0530

    added heropattern

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,14 @@ function App() {
   
   return (
     <>
-      hello 
+      <div className="relative h-screen flex items-center justify-center text-white bg-hero-pattern">
+        
+        <div className="text-center">
+          <h1 className="text-4xl font-bold">Welcome to My Portfolio</h1>
+          <p className="mt-2 text-lg text-gray-300">Showcasing my work & skills</p>
+        </div>
+      </div>
+
     </>
   )
 }
